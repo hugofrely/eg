a135228de00973ad080fe71add291355ca70b593
Hugo Frely <hugo@frely.fr>
2024-07-10 09:28:31 +0200

    feat: game code

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const correctCode = "6784";
+let enteredCode = "";
+
+function addNumber(num) {
+  if (enteredCode.length < 4) {
+    enteredCode += num;
+    document.getElementById("code").value = enteredCode;
+  }
+}
+
+function clearCode() {
+  enteredCode = "";
+  document.getElementById("code").value = enteredCode;
+  document.getElementById("message").innerText = "";
+}
+
+function checkCode() {
+  if (enteredCode === correctCode) {
+    document.getElementById("code-keyboard").remove();
+    const element = document.getElementById("message");
+    element.style.display = "block";
+  } else {
+    document.getElementById("error").innerText =
+      "Code incorrect. Essayez encore.";
+    document.getElementById("error").style.color = "red";
+  }
+}
